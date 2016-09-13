ee139f396068b6cae9dea283cba7a7078ad3f614
Matt Smith <msmith@apaxsoftware.com>
2016-09-13 10:40:36 -0400

    Setup routes.

--- app/views.py
+++ app/views.py
@@ -3,12 +3,13 @@ from flask import render_template
 from app import app
 
 @app.route("/")
-@app.route("/index")
 def index():
-    user = {
-        "full_name": "Matt Smith",
-        "username": "mattsmith1",
-    }
+    return render_template("index.html")
 
+@app.route("/current")
+def current_weather():
+    return render_template("current.html")
 
-    return render_template("index.html", user=user)
+@app.route("/forecast")
+def forecast_weather():
+    return render_template("forecast.html")
