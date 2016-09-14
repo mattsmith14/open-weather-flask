2c3b67af497f9f60d9ca00aec14f53d4f2e09095
Matt Smith <msmith@apaxsoftware.com>
2016-09-14 11:38:38 -0400

    Create DailyWeather object.

--- app/open_weather_api.py
+++ app/open_weather_api.py
@@ -31,5 +31,42 @@ class OpenWeatherAPI():
         payload = self.get_payload(q=q)
         url = "{}{}".format(self.base_url, "/weather")
         r = requests.get(url, params=payload)
+        result = r.json()
+        return DailyWeather(
+                description = result['weather'][0]['description'],
+                icon = result['weather'][0]['icon'],
+                temp = result['main']['temp'],
+                dt = result['dt'],
+                wind = result['wind']['speed'],
+                pressure = result['main']['pressure'],
+                humidity = result['main']['humidity'],
+                sunrise = result['sys']['sunrise'],
+                sunset = result['sys']['sunset']
+            )
 
-        return r.json()
+class DailyWeather():
+    def __init__(self,
+        description="",
+        icon=None,
+        dt=None,
+        temp=None,
+        temp_night=None,
+        wind=None,
+        pressure=None,
+        humidity=None,
+        sunrise=None,
+        sunset=None):
+
+        self.description = description
+        self.icon = icon
+        self.dt = dt
+        self.temp = temp
+        self.temp_night = temp_night
+        self.wind = wind
+        self.pressure = pressure
+        self.humidity = humidity
+        self.sunrise = sunrise
+        self.sunset = sunset
+
+    def __str__(self):
+        return "Description: {}, Temp: {}".format(self.description, self.temp)
