617228eca28850a814ad854e424dddc35f28975c
Matt Smith <msmith@apaxsoftware.com>
2016-09-14 13:55:32 -0400

    Filters to format date and time.

---
+++ app/filters.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from app import app
+
+@app.template_filter('timestamp_to_date')
+def timestamp_to_date(timestamp):
+    date = datetime.fromtimestamp(int(timestamp))
+    return date.strftime("%m/%d/%Y")
+
+@app.template_filter('timestamp_to_time')
+def timestamp_to_time(timestamp):
+    date = datetime.fromtimestamp(int(timestamp))
+    return date.strftime("%I:%M:%S %p")
