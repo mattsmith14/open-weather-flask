5fc5450aeab339f68c77d3e3955bf36467987ac6
Matt Smith <msmith@apaxsoftware.com>
2016-09-14 10:39:49 -0400

    Load config

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
+import sys
+
 from app import app
 
+try:
+    import settings
+except ImportError:
+    print("Please copy settings-dist.py to settings.py and update your API key")
+    sys.exit()
+
+app.config.from_object(settings)
 app.run(debug=True)
